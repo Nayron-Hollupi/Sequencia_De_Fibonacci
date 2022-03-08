7bd6c9bc1150cb50113478000ec6e2d1fb823584
Nayron Hollupi <nhollupi@gmail.com>
2022-03-07 21:21:11 -0300

    Adicionar arquivos de projeto.

---
+++ Sequencia_De_Fibonacci/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Sequencia_De_Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, i, f1 = 0, f2 = 1, f3;
+
+            Console.WriteLine("\nDigite um numero inteiro : ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nA sequencia fibonacci de " + numero +" eh : ");
+            for (i = 0; i < numero; i++)
+            {
+                if(i == numero-1)
+                {
+                    Console.Write(f1 );
+                }
+                else
+                {
+                    Console.Write( f1+"," );
+                }
+               
+                if (i < 2)
+                {
+                    for (i = 0; i < 2; i++)
+                    {
+                        f1 = 1;
+                        Console.Write(f1 +",");
+                    }
+                }
+
+                f3 = f1 + f2;
+                f2 = f1;
+                f1 = f3;
+
+            }
+
+        }
+    }
+}
